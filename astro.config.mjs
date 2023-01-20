0cfffe758efffa7b4998b9c506bf23211a5fec20
Pedro <me@axyria.dev>
2023-01-20 14:02:56 -0300

    feat: cursors and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-    site: "https://runtimee.ee",
+  site: "https://runtimee.ee",
+  integrations: [robotsTxt(), sitemap()]
 });
